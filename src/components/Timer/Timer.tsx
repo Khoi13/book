508ea49dba5e298a60f0ae9a69fb0db3628616dd
Khoi13 <101821382+Khoi13@users.noreply.github.com>
2023-05-15 17:42:20 +0700

    Update Timer.tsx

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -41,18 +41,18 @@ function Timer({
     const { days, hours, minutes, seconds, pass } = timeLeft;
      
     const MonthNum = {
-        January:   01,
-        February:  02,  
-        March:     03, 
-        April:     04, 
-        May:       05, 
-        June:      06, 
-        July:      07, 
-        August:    08, 
-        September: 09, 
-        October:   10, 
-        November:  11, 
-        December:  12
+        January:   '01',
+        February:  '02',  
+        March:     '03', 
+        April:     '04', 
+        May:       '05', 
+        June:      '06', 
+        July:      '07', 
+        August:    '08', 
+        September: '09', 
+        October:   '10', 
+        November:  '11', 
+        December:  '12'
     }                     
                           
     setTimeout(() => {
